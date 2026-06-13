bae82b83a7a16fb9e182581c9073cdf8d7d2acf6
Rajat Trehan <trehanrajat4@gmail.com>
2026-06-14 00:03:48 +0530

    pipeline implemented

--- llm_explainer.py
+++ llm_explainer.py
@@ -11,8 +11,40 @@ genai.configure(
 model = genai.GenerativeModel(
     "gemini-2.5-flash"
 )
+def build_repo_prompt(question , repo_context):
+    context = ""
 
-def build_prompt(question ,  chunks):
+    for file in repo_context:
+        context += f"""
+========== FILE ==========
+Path:
+{file["path"]}
+
+Code:
+{file["content"]}
+
+"""
+
+    prompt = f"""
+You are an expert AI software engineer & an expert software architect helping a developer understand their repository.
+Answer repository-level questions using the provided repository information.
+CRITICAL INSTRUCTIONS FOR HANDLING USER INPUT:
+1. CASUAL CHAT / GREETINGS: If the user says hello, asks how you are, or makes casual small talk, respond politely and naturally as an AI assistant. You do not need to look at the repository context for these.
+2. REPO QUESTIONS: If the user asks about the code, architecture, or features, use the provided "Repository Context" below to answer accurately. 
+3. OUT OF SCOPE: If they ask something completely unrelated to the code or casual chat (e.g., "What is the capital of France?"), politely remind them that you are here to help them with their codebase.
+
+User's Question:
+{question}
+
+Repository Context:
+{context}
+
+Provide a clear, direct response based on the rules above.
+
+"""
+    return prompt
+
+def build_feature_prompt(question ,  chunks):
     context = ""
 
     for chunk in chunks:
@@ -25,29 +57,13 @@ Code:
 {chunk["content"]}
 
 """
-    
-# #     prompt = f"""
-# # You are an expert software engineer.
-
-# # Answer the user's question using ONLY the provided repository code.
-
-# # Question:
-# # {question}
-
-# # Repository Context:
-# # {context}
-
-# # Provide a clear explanation.
-# # """
-    
-#     return prompt
 
     prompt = f"""
 You are an expert AI software engineer assistant helping a developer understand their repository.
 
 CRITICAL INSTRUCTIONS FOR HANDLING USER INPUT:
 1. CASUAL CHAT / GREETINGS: If the user says hello, asks how you are, or makes casual small talk, respond politely and naturally as an AI assistant. You do not need to look at the repository context for these.
-2. REPO QUESTIONS: If the user asks about the code, architecture, or features, use the provided "Repository Context" below to answer accurately. 
+2. FEATURE QUESTIONS: If the user asks about the code, architecture, or features, use the provided "Repository Context" below to answer accurately. 
 3. OUT OF SCOPE: If they ask something completely unrelated to the code or casual chat (e.g., "What is the capital of France?"), politely remind them that you are here to help them with their codebase.
 
 User's Question:
@@ -61,7 +77,14 @@ Provide a clear, direct response based on the rules above.
     return prompt
 
 def explain_code(question , chunks):
-    prompt = build_prompt(question , chunks)
+    prompt = build_feature_prompt(question , chunks)
+
+    response = model.generate_content(prompt)
+
+    return response.text
+
+def explain_repo(question , repo_context):
+    prompt = build_repo_prompt(question , repo_context)
 
     response = model.generate_content(prompt)
 
--- script.py
+++ script.py
@@ -1,54 +1,67 @@
 import repo_downloader
 import file_reader
 import chunker
-# import embedding_generator
-# import retriever
-# import llm_explainer
+import embedding_generator
+import retriever
+import llm_explainer
 import question_classifier
-import repository_context
+import repo_context
 import storage
 import utils
 import os
 
-if os.path.exists("repo_info.json"):
-    print("found repo yes")
+choice = input(
+    f"""
+1. Index Repository
+2. Ask Questions
 
-else :
-    print("no repo found")
-# repo_url = "https://github.com/Rajat072005/SyncSphere-Website"
-# folder_name = "sample_repo"
+Enter Your Choice : 
+"""
+)
 
-# repo_downloader.download_repo(repo_url , folder_name)
-# files = file_reader.read_repository(folder_name)
-# chunks = chunker.create_chunks(files)
+if(choice == "1"):
+    repo_url = "https://github.com/Rajat072005/SyncSphere-Website"
+    folder_name = "sample_repo"
+    repo_downloader.download_repo(repo_url , folder_name)
+    files = file_reader.read_repository(folder_name)
+    repository_context = repo_context.build_repo_context(files)
+    chunks = chunker.create_chunks(files)
+    #
+    embeddings = embedding_generator.generate_embeddings(chunks)
+    storage.save_json(repository_context , "repo_context.json")
+    storage.save_json(chunks , "chunks.json")
+    storage.save_json(embeddings , "embeddings.json")
+
+elif(choice == "2"):
+    chunks = storage.load_json("chunks.json")
+    embeddings = storage.load_json("embeddings.json")
+    chunk_map = utils.build_chunkmap(chunks)
+    question = input(
+    "Ask a question about the repository: "
+    )
+    question_type = question_classifier.question_classifier(question)
+    if question_type =="repository":
+        results = storage.load_json("repo_context.json")
+        answer = llm_explainer.explain_repo(question,results) 
+    else:
+        results = retriever.retrieve(question , embeddings ,chunk_map, top_k = 3 )
+        answer = llm_explainer.explain_code(question,results)
 
 
-# choice = input("""
-# 1. Index Repository
-# 2. Ask Questions
-            
-# Enter Choice :
-# """)
+    print(answer)
 
-# if choice == '1':
-#     print("Indexing Mode")
-# elif choice == '2':
-#     print('Question Mode')
+#
+#
+#
+#
 
-#embeddings = embedding_generator.generate_embeddings(chunks)
+# 
+# 
 
-# question = input(
-#     "Ask a question about the repository: "
-# )
 
-# question_type = question_classifier.question_classifier(question)
-# if question_type =="repository":
-#     results = repository_context.get_repository_context(files)
-# else:
-#     results = retriever.retrieve(question , embeddings ,chunk_map, top_k = 3 )
-# answer = llm_explainer.explain_code(question,results)
 
-# print(answer)
+
+
 
 
 
