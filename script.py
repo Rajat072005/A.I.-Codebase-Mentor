d16fff39f5caf5467d79e72b2007ecb2d9dfe963
Rajat Trehan <trehanrajat4@gmail.com>
2026-06-17 15:03:30 +0530

    retrieverb logic updated

--- script.py
+++ script.py
@@ -19,26 +19,26 @@ Enter Your Choice : """
 )
 
 
-# if(choice == "1"):
-#     # repo_url = "https://github.com/Rajat072005/SyncSphere-Website"
-#     repo_url = input("Provide Github Repository Url : ")
-#     repo_name = utils.extract_repo_name(repo_url)
-#     repo_folder = utils.create_repo_folder(repo_name)
-#     repo_code_folder = f"{repo_folder}/repository"
-#     repo_info = {
-#         "repo_name" : repo_name,
-#         "repo_url" : repo_url
-#     }
-#     storage.save_json(repo_info , f"{repo_folder}/repo_info.json")
-#     # folder_name = "sample_repo"
-#     repo_downloader.download_repo(repo_url , repo_code_folder)
-#     files = file_reader.read_repository(repo_code_folder)
-#     repository_context = repo_context.build_repo_context(files)
-#     chunks = chunker.create_chunks(files)
-#     embeddings = embedding_generator.generate_embeddings(chunks)
-#     storage.save_json(repository_context , f"{repo_folder}/repo_context.json")
-#     storage.save_json(chunks , f"{repo_folder}/chunks.json")
-#     storage.save_json(embeddings , f"{repo_folder}/embeddings.json")
+if(choice == "1"):
+    # repo_url = "https://github.com/Rajat072005/SyncSphere-Website"
+    repo_url = input("Provide Github Repository Url : ")
+    repo_name = utils.extract_repo_name(repo_url)
+    repo_folder = utils.create_repo_folder(repo_name)
+    repo_code_folder = f"{repo_folder}/repository"
+    repo_info = {
+        "repo_name" : repo_name,
+        "repo_url" : repo_url
+    }
+    storage.save_json(repo_info , f"{repo_folder}/repo_info.json")
+    # folder_name = "sample_repo"
+    repo_downloader.download_repo(repo_url , repo_code_folder)
+    files = file_reader.read_repository(repo_code_folder)
+    repository_context = repo_context.build_repo_context(files)
+    chunks = chunker.create_chunks(files)
+    embeddings = embedding_generator.generate_embeddings(chunks)
+    storage.save_json(repository_context , f"{repo_folder}/repo_context.json")
+    storage.save_json(chunks , f"{repo_folder}/chunks.json")
+    storage.save_json(embeddings , f"{repo_folder}/embeddings.json")
 
 if(choice == "2"):
     repos = utils.get_saved_repo()
@@ -61,24 +61,24 @@ Select Repository : """
         question = input(
         "Ask a question about the repository: "
         )
-        # question_type = question_classifier.question_classifier(question)
-        # if question_type == "casual":
-        #     answer = llm_explainer.explain_casual(question)
-        # elif question_type =="repository":
-        #     results = storage.load_json(f"{repo_folder}/repo_context.json")
-        #     answer = llm_explainer.explain_repo(question,results) 
-        # else:
-        #     results = retriever.retrieve(question , embeddings ,chunk_map, top_k = 3 )
-        #     answer = llm_explainer.explain_code(question,results)
-        results = retriever.retrieve(question , embeddings ,chunk_map, top_k = 3 )
+        question_type = question_classifier.question_classifier(question)
+        if question_type == "casual":
+            answer = llm_explainer.explain_casual(question)
+        elif question_type =="repository":
+            results = storage.load_json(f"{repo_folder}/repo_context.json")
+            answer = llm_explainer.explain_repo(question,results) 
+        else:
+            results = retriever.retrieve(question , embeddings ,chunk_map, top_k = 3 )
+            answer = llm_explainer.explain_code(question,results)
+        # results = retriever.retrieve(question , embeddings ,chunk_map, top_k = 3 )
 
-        for index, result in enumerate (results , start = 1):
-            print(f"Retrieved File {index} : {result['path']}")
+        # for index, result in enumerate (results , start = 1):
+        #     print(f"Retrieved File {index} : {result['path']}")
         
-        # answer = llm_explainer.explain_code(question,results)
+        answer = llm_explainer.explain_code(question,results)
 
 
-        # print(answer)
+        print(answer)
 
     
 
